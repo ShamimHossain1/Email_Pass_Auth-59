1b1aec111f0eca810b642b55ee14b0c69dbed163
ShamimHossain1 <beastb697@gmail.com>
2023-04-18 03:20:52 +0600

    login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,72 @@
-import React from 'react';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import app from '../Firebase/Firebase';
 
+const auth = getAuth(app);
 const Login = () => {
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        setError('');
+        setSuccess('');
+        if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+            setError('Please add at least two uppercase.');
+            return;
+        }
+        else if (!/(?=.*[!@#$&*])/.test(password)) {
+            setError('Please add at least one special character');
+            return;
+        }
+        else if (password.length < 6) {
+            setError('Password must be 6 character long');
+            return;
+        }
+
+        signInWithEmailAndPassword(auth, email, password)
+
+            .then(result => {
+                const loggedUser = result.user;
+                setSuccess('User Login Successful');
+                setError('');
+                form.reset();
+
+            })
+
+            .catch(error => {
+                setError(error.message)
+            })
+
+    }
+
     return (
-        <div className='text-center'>
-            <h2>login</h2>
+        <div className='text-center border p-5 w-1/2 mx-auto'>
+            <h2 className='font-semibold text-xl mb-4'>please Login</h2>
+            <form onSubmit={handleLogin} className="mx-auto max-w-md">
+                <div className="mb-4">
+                    <label htmlFor="email" className="block text-gray-700 font-bold mb-2">Email Address</label>
+                    <input type="email" id="email" name="email" className="w-full px-4 py-3 rounded-lg shadow-sm focus:outline-none focus:shadow-outline-gray" placeholder="Enter your email address" required />
+                </div>
+                <div className="mb-6">
+                    <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password</label>
+                    <input type="password" id="password" name="password" className="w-full px-4 py-3 rounded-lg shadow-sm focus:outline-none focus:shadow-outline-gray" placeholder="Enter your password" required />
+                </div>
+                <div className="flex items-center justify-between">
+                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue">Sign In</button>
+                    <Link to='/register'><a href="#" className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">Register</a></Link>
+                   
+                </div>
+                <p className='text-green-500 font-semibold'>{success}</p>
+                <p className='text-red-600'>{error}</p><br />
+
+            </form>
+
         </div>
     );
 };
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from '../Firebase/Firebase';
+import { Link } from 'react-router-dom';
 
 const auth = getAuth(app);
 
 const Register = () => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
+    const [error, setError]= useState('');
+    const [success, setSuccess] = useState('')
 
 
     const handleEmailChange = (event) =>{
@@ -19,27 +22,53 @@ const Register = () => {
     }
     const handleSubmit = (event) =>{
         event.preventDefault();
+        setSuccess('');
+        setError('');
         const email = (event.target.email.value)
         const password = (event.target.password.value)
+
+        // validate
+
+        if(!/(?=.*[A-Z].*[A-Z])/.test(password)){
+            setError('Please add at least two uppercase.');
+            return;
+        }
+        else if(!/(?=.*[!@#$&*])/.test(password)){
+            setError('Please add at least one special character');
+            return;
+        }
+        else if (password.length <6){
+            setError('Password must be 6 character long');
+            return;
+        }
 
         createUserWithEmailAndPassword(auth, email, password)
         .then (result=>{
             const loggedUser = result.user;
             console.log(loggedUser);
+            setError('')
+            event.target.reset()
+            setSuccess('Register SuccessFull')
         })
         .catch(error =>{
-            console.log(error);
+            // console.log(error.message);
+            setError(error.message)
         })
 
     }
     return (
         <div className='text-center'>
-            <h2 >Please Register</h2>
+            <h2  className='font-semibold text-xl mb-4' >Please Register</h2>
             <form onSubmit={handleSubmit} className='mt-5 border py-5 w-1/4 mx-auto rounded-lg'>
-                <input onChange={handleEmailChange} type="email" placeholder='Your Email' name='email' id='email' className='rounded-lg' /><br /><br />
-                <input onBlur={HandlePasswordBlur} type="password" name='password' id='password' placeholder='Your Password' className='rounded-lg' /><br /><br />
+                <input onChange={handleEmailChange} type="email" placeholder='Your Email' name='email' id='email' className='rounded-lg' required /><br /><br />
+                <input onBlur={HandlePasswordBlur} type="password" name='password' id='password' placeholder='Your Password' className='rounded-lg' required /><br />
+                <p className='text-red-600'>{error}</p><br />
+
                 <input type="submit" value="Register" className='border p-2 rounded-lg ' />
+                <p>Already Have an account? Please <Link className='text-blue-600 underline font-semibold' to='/login'>Login</Link></p>
+                <p className='text-green-500 font-semibold'>{success}</p>
             </form>
+            
         </div>
     );
 };
